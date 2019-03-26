d787f8b80b695d05fe4f0976f0a5835bdb4030f3
Ruben Gees <rubengees7@googlemail.com>
2019-03-26 15:19:55 +0100

    Add reproduction case

---
+++ src/components/ScrollSample.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout
+        justify-center
+        row
+        wrap
+    >
+      <v-flex xs12>
+        <v-btn @click="dialog = true">Show dialog</v-btn>
+      </v-flex>
+
+      <v-flex xs12 :key="i" v-for="i in sampleData">{{ i }}</v-flex>
+    </v-layout>
+    <v-dialog v-model="dialog">Dialog</v-dialog>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      sampleData: Array.apply(null, {length: 100}).map(Number.call, Number),
+    }),
+  }
+</script>
+
+<style>
+
+</style>
